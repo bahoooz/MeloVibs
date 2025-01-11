43a61d73e314b1c84e9f958f2984b3b374c02ed5
Bahoz <julienbraultcherif@gmail.com>
2025-01-11 22:06:25 +0100

    fix logic feature for bypass vercel max delay

--- lib/updateAllArtists.ts
+++ lib/updateAllArtists.ts
@@ -1,25 +1,28 @@
 export async function updateAllArtists() {
-    const genres = [
-      'rap-fr', 'pop', 'jazz', 'r&b', 'afro-beats',
-      'rap-us', 'latines', 'rock', 'electro', 'kpop'
-    ];
-  
-    try {
-      const updatePromises = genres.map(async (genre) => {
+  const genres = [
+    'rap-fr', 'pop', 'jazz', 'r&b', 'afro-beats',
+    'rap-us', 'latines', 'rock', 'electro', 'kpop'
+  ];
+
+  try {
+    for (const genre of genres) {
+      try {
         const response = await fetch(`/api/spotify/get-list-artists/${genre}`);
         if (!response.ok) {
-            throw new Error(`Erreur lors de la récupération des artistes Spotify ${genre}: ${response.statusText}`);
+          console.error(`Erreur pour ${genre}: ${response.statusText}`);
+          continue; // Continue avec le prochain genre même en cas d'erreur
         }
         const data = await response.json();
-        console.log(`Données des artistes Spotify ${genre}:`, data);
-        return data;
-      });
-  
-      await Promise.all(updatePromises);
-      console.log("Mise à jour de toutes les artistes terminée");
-    } catch (error) {
-      console.error("Erreur lors de la mise à jour des artistes:", error);
-      throw error;
+        console.log(`Données des artistes Spotify ${genre} mises à jour :`, data);
+      } catch (error) {
+        console.error(`Erreur pour ${genre}:`, error);
+        continue; // Continue avec le prochain genre même en cas d'erreur
+      }
     }
+    console.log("Mise à jour de tous les artistes terminée");
+  } catch (error) {
+    console.error("Erreur lors de la mise à jour des artistes:", error);
+    throw error;
   }
+}
   
--- lib/updateAllTracks.ts
+++ lib/updateAllTracks.ts
@@ -5,17 +5,20 @@ export async function updateAllTracks() {
   ];
 
   try {
-    const updatePromises = genres.map(async (genre) => {
-      const response = await fetch(`/api/spotify/get-list-tracks/${genre}`);
-      if (!response.ok) {
-        throw new Error(`Erreur lors de la récupération des pistes Spotify ${genre}: ${response.statusText}`);
+    for (const genre of genres) {
+      try {
+        const response = await fetch(`/api/spotify/get-list-tracks/${genre}`);
+        if (!response.ok) {
+          console.error(`Erreur pour ${genre}: ${response.statusText}`);
+          continue; // Continue avec le prochain genre même en cas d'erreur
+        }
+        const data = await response.json();
+        console.log(`Données des pistes Spotify ${genre} mises à jour :`, data);
+      } catch (error) {
+        console.error(`Erreur pour ${genre}:`, error);
+        continue; // Continue avec le prochain genre même en cas d'erreur
       }
-      const data = await response.json();
-      console.log(`Données des pistes Spotify ${genre}:`, data);
-      return data;
-    });
-
-    await Promise.all(updatePromises);
+    }
     console.log("Mise à jour de toutes les pistes terminée");
   } catch (error) {
     console.error("Erreur lors de la mise à jour des pistes:", error);
